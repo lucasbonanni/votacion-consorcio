baf556d2836d6ad1d5d86d0bbcdb632fb11c5785
danag <le_bonanni@yahoo.com.ar>
2018-05-04 23:16:42 -0300

    se agrega logica a los slides

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,61 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+
+  @ViewChild('slides') slidesRef: Slides;
+  voto: boolean = false;
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ngOnInit(): void {
+    // throw new Error("Method not implemented.");
+    // this.slidesRef.lockSwipes(this.voto);
+    this.slidesRef.enableKeyboardControl(true);
+  }
+
+  //   slides = [
+  //     {
+  //       title: "Bienvenido!!",
+  //       description: "Bievenido a la aplicación de votación de consorcio.",
+  //       image: "assets/img/ica-slidebox-img-1.png",
+  //     },
+  //     {
+  //       title: "What is Ionic?",
+  //       description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
+  //       image: "assets/img/ica-slidebox-img-2.png",
+  //     },
+  //     {
+  //       title: "What is Ionic Cloud?",
+  //       description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+  //       image: "assets/img/ica-slidebox-img-3.png",
+  //     }
+  // ];.
+
+  slideChanged() {
+    let currentIndex = this.slidesRef.getActiveIndex();
+    console.log('current slide', currentIndex);
+    if(currentIndex === 1 && !this.voto){
+      this.slidesRef.lockSwipes(true);
+      this.slidesRef.lockSwipeToNext
+    }
+    if(!this.voto && currentIndex === 2){
+      this.slidesRef.slidePrev();
+    }
+  }
+
+  mensage() {
+    console.log('prueba botón');
+    this.slidesRef.lockSwipes(false);
+    this.slidesRef.slideNext();
+    this.slidesRef.lockSwipes(true);
   }
 
 }
