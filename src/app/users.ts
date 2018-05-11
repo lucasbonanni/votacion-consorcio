4d8f27de68031cc513bcb2ad63412d1922cd810a
Lucas Bonanni <lucas_eb2004@yahoo.com.ar>
2018-05-11 09:12:25 -0300

    se agrega constante con los usuarios de testing

---
+++ src/app/users.ts
@@ -0,0 +1,7 @@
+export const usuarios = [
+    { id: 1, nombre: 'admin@gmail.com', clave: '123456', perfil: 'admin', sexo: 'Female' },
+    { id: 2, nombre: 'invitado@gmail.com', clave: '123456', perfil: 'invitado', sexo: 'Female' },
+    { id: 3, nombre: 'usuario@gmail.com', clave: '123456', perfil: 'usuario', sexo: 'Male' },
+    { id: 4, nombre: 'anonimo@gmail.com', clave: '123456', perfil: 'usuario', sexo: 'Male' },
+    { id: 5, nombre: 'tester@gmail.com', clave: '123456', perfil: 'tester', sexo: 'Female' }
+]
