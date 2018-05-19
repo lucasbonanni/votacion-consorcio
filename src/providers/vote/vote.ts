ab595fe3684d223a6a95d8264178a6e9ae95dcef
Lucas Bonanni <lucas_eb2004@yahoo.com.ar>
2018-05-19 04:46:47 -0300

    se agrega icono y splash screen

--- src/providers/vote/vote.ts
+++ src/providers/vote/vote.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
-import { checkNoChangesNode } from '@angular/core/src/view/view';
 import 'rxjs/add/operator/toPromise';
 import { voteModel, voteResultModel } from '../../models/voteModel';
 /*
